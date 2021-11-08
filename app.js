bf64f770f474b392a26c11c29999c752f9c39de1
shashankkatte <shashankanand.katte@gmail.com>
2021-11-07 20:04:09 -0500

    src : Demo adding 3rd party npm package - chalk

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const validator = require('validator');
+const chalk = require('chalk');
+
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 
 console.log(msg);
 
-console.log(validator.isEmail('john@doe.com'));
-console.log(validator.isURL('https://www.katte.io'));
+console.log(chalk.green('Success!'));
