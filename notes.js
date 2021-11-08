a3d6d8d75055a63f47ed29ffa572c9036c6f12ad
shashankkatte <shashankanand.katte@gmail.com>
2021-11-07 23:10:24 -0500

    src : Listing Notes functionality

--- notes.js
+++ notes.js
@@ -33,6 +33,16 @@ const removeNote = (title) => {
   }
 };
 
+const listNotes = () => {
+  const notes = loadNotes();
+
+  console.log(chalk.inverse('Your notes'));
+
+  notes.forEach((note) => {
+    console.log(note.title);
+  });
+};
+
 const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
@@ -52,4 +62,5 @@ module.exports = {
   getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
+  listNotes: listNotes,
 };
